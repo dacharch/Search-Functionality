922af61b3f505407afc91fe3eaba17c71b5c2fc7
Neeraj Kumar <nk35715945@gmail.com>
2024-07-29 12:30:36 +0530

    Update the files

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,13 @@ function App() {
   return (
     <div className="container">
       <div className='search-container'>
-         <h2>Search your Shoes </h2>
+         <h2>Search your shoes </h2>
         <input className='search-bar' type="text" placeholder='Search Here' />
         <div className='btn'>
           <button>Search Result</button>
+        </div>
+        <div className='output'>
+
         </div>
       </div>
     </div>
